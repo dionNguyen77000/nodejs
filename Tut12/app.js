340e14aafb05422c76bc3b40482510f76f32b1e8
Dion Nguyen <nguyed22@qut.edu.au>
2016-07-08 15:21:09 +1000

    adding node js done until tut 13 to git hub

---
+++ Tut12/app.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+//fs.writeFileSync("corn.txt", "Corn is good, corn is life");
+
+//console.log(fs.readFileSync("corn.txt").toString());
+
+var path = require('path');
+/*var websiteHome = "H:/node.js/index.html";
+var websiteAbout = "H:/node.js/about.html";
+
+console.log(path.normalize(websiteHome));
+console.log(path.dirname(websiteAbout));
+console.log(path.basename(websiteAbout));
+console.log(path.extname(websiteAbout));
+*/
+
+setInterval(function(){
+	console.log("beef");
+}, 2000);
+
+console.log(_dirname);
+console.log(_filename);
